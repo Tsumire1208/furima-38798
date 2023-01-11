f3b28e7be83197179be458c651c6efd8b29b5e5a
Tsumire1208 <utamiori20210117@gmail.com>
2023-01-11 20:55:13 +0900

    usersテーブル作成・『新規登録』一部修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,5 +18,5 @@ class User < ApplicationRecord
   end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: '※英字と数字の両方を含めて設定してください'
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Password is invalid. Include both letters and numbers'
 end
