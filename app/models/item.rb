71fedd7e6769c6c90afa6d7ebbca01f5736210fb
Tsumire1208 <utamiori20210117@gmail.com>
2023-01-17 10:54:12 +0900

    単体テスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,16 +9,15 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :delivery_time
 
-
+  validates :image,            presence: true
   validates :name,             presence: true
   validates :item_description, presence: true
-  validates :category_id,      presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :condition_id,     presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :freight_id,       presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefecture_id,    presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :delivery_time_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price,            presence: true
-  validates :image,            presence: true
+  validates :category_id,      presence: true, numericality: { other_than: 1 }
+  validates :condition_id,     presence: true, numericality: { other_than: 1 }
+  validates :freight_id,       presence: true, numericality: { other_than: 1 }
+  validates :prefecture_id,    presence: true, numericality: { other_than: 1 }
+  validates :delivery_time_id, presence: true, numericality: { other_than: 1 }
+  validates :price,            presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   validates :user_id,          presence: true
 
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,83 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品登録' do
+      context '出品登録できるとき' do
+        it 'image, name, item_description, category_id, condition_id, freight_id, prefecture_id, delivery_time_id, priceが存在すれば登録できる' do
+          expect(@item).to be_valid
+        end
+      end
+
+    context '出品登録できないとき' do
+      # it 'imageがないと出品できない' do
+      #   @item.image = nil
+      #   @item.valid?
+      #   expect(@item.errors.full_messages).to include("Image can't be blank")
+      # end
+
+      it 'nameが空では登録できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it 'item_descriptionが空では登録できない' do
+        @item.item_description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item description can't be blank")
+      end
+
+      it 'category_idが---では登録できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category id can't be blank")
+      end
+
+      it 'condition_idが---では登録できない' do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition id can't be blank")
+      end
+
+      it 'freight_idが---では登録できない' do
+        @item.freight_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Freight id can't be blank")
+      end
+
+      it 'prefecture_idが---では登録できない' do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture id can't be blank")
+      end
+
+      it 'delivery_time_idが---では登録できない' do
+        @item.delivery_time_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery time id can't be blank")
+      end
+
+      it 'priceが空欄では登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it 'priceが299円以下では登録できない' do
+        @item.price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it 'priceが10000000円以上では登録できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+    end
+  end
 end
