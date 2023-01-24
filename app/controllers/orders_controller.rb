e306867902308a7dbfbd322ee0cb6ba376e09548
Tsumire1208 <utamiori20210117@gmail.com>
2023-01-24 14:45:32 +0900

    token作成。鍵の環境変数設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -19,7 +19,16 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_buyer).permit(:postal_code, :prefecture_id, :municipalities, :address, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_buyer).permit(:postal_code, :prefecture_id, :municipalities, :address, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 
 end
--- app/models/order_buyer.rb
+++ app/models/order_buyer.rb
@@ -1,7 +1,7 @@
 class OrderBuyer
 
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :municipalities, :address, :building, :phone, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :municipalities, :address, :building, :phone, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
@@ -11,6 +11,7 @@ class OrderBuyer
     validates :phone, format: { with: /\A[0-9]{10,11}\z/}
     validates :user_id
     validates :item_id
+    validates :token
   end
 
   def save
