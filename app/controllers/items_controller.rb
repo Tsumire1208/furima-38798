131d0e98913bd25f67c2e31395d0a5e2237fd8b8
Tsumire1208 <utamiori20210117@gmail.com>
2023-02-01 23:10:11 +0900

    複数画像投稿機能実装②

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -50,8 +50,8 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :item_description, :category_id, :condition_id, :freight_id, :prefecture_id,
-                                 :delivery_time_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :item_description, :category_id, :condition_id, :freight_id, :prefecture_id,
+                                 :delivery_time_id, :price, {images: []}).merge(user_id: current_user.id)
   end
 
   def set_item
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,23 +1,23 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to       :user
-  has_one          :order
-  has_one_attached :image
+  belongs_to        :user
+  has_one           :order
+  has_many_attached :images
 
-  belongs_to       :category
-  belongs_to       :condition
-  belongs_to       :freight
-  belongs_to       :prefecture
-  belongs_to       :delivery_time
+  belongs_to        :category
+  belongs_to        :condition
+  belongs_to        :freight
+  belongs_to        :prefecture
+  belongs_to        :delivery_time
 
-  validates        :image,            presence: true
-  validates        :name,             presence: true
-  validates        :item_description, presence: true
-  validates        :category_id,      presence: true, numericality: { other_than: 1, message: 'は『---』以外を選択してください' }
-  validates        :condition_id,     presence: true, numericality: { other_than: 1, message: 'は『---』以外を選択してください' }
-  validates        :freight_id,       presence: true, numericality: { other_than: 1, message: 'は『---』以外を選択してください' }
-  validates        :prefecture_id,    presence: true, numericality: { other_than: 1, message: 'は『---』以外を選択してください' }
-  validates        :delivery_time_id, presence: true, numericality: { other_than: 1, message: 'は『---』以外を選択してください' }
-  validates        :price,            presence: true,
-                                      numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates         :images,             length: { minimum: 1, maximum: 5, message: "は1枚以上5枚以下にしてください" }
+  validates         :name,             presence: true
+  validates         :item_description, presence: true
+  validates         :category_id,      presence: true, numericality: { other_than: 1, message: 'は『---』以外を選択してください' }
+  validates         :condition_id,     presence: true, numericality: { other_than: 1, message: 'は『---』以外を選択してください' }
+  validates         :freight_id,       presence: true, numericality: { other_than: 1, message: 'は『---』以外を選択してください' }
+  validates         :prefecture_id,    presence: true, numericality: { other_than: 1, message: 'は『---』以外を選択してください' }
+  validates         :delivery_time_id, presence: true, numericality: { other_than: 1, message: 'は『---』以外を選択してください' }
+  validates         :price,            presence: true,
+                                       numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 end
